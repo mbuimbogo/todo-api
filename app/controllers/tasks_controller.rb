02127cfe204d9bbe654bc9460bef79ce311e6693
mbuimbogo <mbuimbogo99@gmail.com>
2023-05-28 00:46:42 +0300

    Add update, destroy and create methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,50 @@
 class TasksController < ApplicationController
-    def index
-        @tasks = Task.all
-        render json: @tasks
-      end
+        rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+      
+        # GET /tasks
+        def index
+          tasks = Task.all
+          render json: tasks
+        end
+      
+        # POST /tasks
+        def create
+          task = Task.create(task_params)
+          render json: task, status: :created
+        end
+      
+        # GET /tasks/:id
+        def show
+          task = find_task
+          render json: task
+        end
+      
+        # PATCH /tasks/:id
+        def update
+          task = find_task
+          task.update(task_params)
+          render json: task
+        end
+      
+        # DELETE /tasks/:id
+  def destroy
+    task = find_task
+    task.destroy
+    head :no_content
+  end
+
+  private
+
+  def find_task
+    Task.find(params[:id])
+  end
+
+  def task_params
+    params.permit(:description, :completed)
+  end
+
+  def render_not_found_response
+    render json: { error: "Task not found" }, status: :not_found
+  end
+      
 end
