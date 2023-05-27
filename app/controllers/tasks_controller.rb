2c5c785bc7a0c81b28ad98b66f634550834da338
mbuimbogo <mbuimbogo99@gmail.com>
2023-05-28 01:04:22 +0300

    Add rescue from for error handling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
         rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
         wrap_parameters format: []
+        rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
         # GET /tasks
         def index
@@ -10,20 +11,20 @@ class TasksController < ApplicationController
       
         # POST /tasks
         def create
-          task = Task.create(task_params)
+          task = Task.create!(task_params)
           render json: task, status: :created
         end
       
         # GET /tasks/:id
-        def show
-          task = find_task
-          render json: task
-        end
+        # def show
+        #   task = find_task
+        #   render json: task
+        # end
       
         # PATCH /tasks/:id
         def update
           task = find_task
-          task.update(task_params)
+          task.update!(task_params)
           render json: task
         end
       
@@ -48,4 +49,7 @@ class TasksController < ApplicationController
     render json: { error: "Task not found" }, status: :not_found
   end
       
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
 end
