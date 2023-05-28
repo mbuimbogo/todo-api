4804fbef0adb316050302df6cd898b1b4ec96eae
mbuimbogo <mbuimbogo99@gmail.com>
2023-05-28 14:27:15 +0300

    add route to root index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 
 Rails.application.routes.draw do
   resources :tasks, except: [:new, :edit]
-
+  root to: 'tasks#index'
   match '/tasks', to: 'tasks#options', via: :options
   delete '/tasks/:id', to: 'tasks#destroy'
   
 end
+
+
